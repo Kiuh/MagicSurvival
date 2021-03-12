40d9801edde198dc24eb1692447ac9c25bba09b2
Коля Химич <30412223+Kiuh@users.noreply.github.com>
2021-03-12 06:43:48 +0200

    СhooseClassV0.1

--- MagicSurvival/Assets/Scripts/Menu/GlobalInfo.cs
+++ MagicSurvival/Assets/Scripts/Menu/GlobalInfo.cs
@@ -5,6 +5,33 @@ using UnityEngine;
 public class GlobalInfo : MonoBehaviour
 {
     public static int points = 0;
+    string[,] descriptions = new string[24, 2]
+    {
+        { "Archemist","" },
+        { "Archmage","" },
+        { "Astronomer","" },
+        { "Bard","" },
+        { "Battle Mage","" },
+        { "Bishop","" },
+        { "Dark Mage","" },
+        { "Druid","" },
+        { "Electromancer","" },
+        { "Elementalist","" },
+        { "Enchantress","" },
+        { "Invoker","" },
+        { "Kabbalist","" },
+        { "Lich","" },
+        { "Mage","" },
+        { "Magician","" },
+        { "Scholar","" },
+        { "Shaman","" },
+        { "Sorcer","" },
+        { "Summoner","" },
+        { "Warlock","" },
+        { "Warlord","" },
+        { "Witch","" },
+        { "Wizard","" }
+    };
     private void Awake()
     {
         GameObject.DontDestroyOnLoad(this.gameObject);
